fd2dec8d59e7e2c1faee259e2541c3871025963a
RamiroFerrari <ramiroferrariadrian@gmail.com>
2024-05-13 08:08:05 -0300

    tp exepciones

---
+++ miPrimeraApi/src/main/java/org/example/miprimeraapi/entities/Persona.java
@@ -0,0 +1,29 @@
+package org.example.miprimeraapi.entities;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.StandardException;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="persona")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Persona implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @Column(name="dni")
+    private int dni;
+}
---
+++ miPrimeraApi/src/test/java/org/example/miprimeraapi/MiPrimeraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miprimeraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimeraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
